8d57e9f24ac532a2607b0ece03718f2082e3b965
Daryl Rogony <darylrogony@gmail.com>
2022-04-07 13:17:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+  //console.log(`This saturday, I want to ${activity}!`);
+}
+function mondayWork (activity2 = "go to the office") {
+  return `This Monday, I will ${activity2}.`;
+}
+
+
+let wrapAdjective = function(style="a good programmer") {
+  return function(adjective = "special") {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
+
